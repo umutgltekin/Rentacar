025698465b39e48d38fc9d03bb534dd04825ed7d
Umut Gültekin <82710724+gltkn23@users.noreply.github.com>
2022-01-03 02:17:29 +0300

    Add files via upload

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+            new Product{ProductId=1,CategoryId=1, ProductName="Mercedes",UnitInstock=15,UnitPrice=15},
+            new Product{ProductId=2,CategoryId=2, ProductName="Posrsch",UnitInstock=15,UnitPrice=15},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete=null;
+
+            productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+           
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitInstock = product.UnitInstock;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.CategoryId = product.CategoryId;
+
+                }
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+
+        }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstarct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+   public  class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public String ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public short UnitInstock { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
